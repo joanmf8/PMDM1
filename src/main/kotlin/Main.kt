4df94ee15203bca3d86aa6607f0c2bc60d74c486
joanmf8 <joanmf8@gmail.com>
2020-09-22 17:22:38 +0200

    Completed exercise 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main() {
     printStringLength(null)
     //println("Concatenation: " + concatenate("hello", null))
     exercise1()
+    exercise2()
 }
 
 // TODO: 1.- Write a function called exercise1()
@@ -20,7 +21,7 @@ fun exercise1(){
 }
 
 // TODO: 2.- Uncomment this function:
-/* fun exercise2() {
+ fun exercise2() {
     var month = 1
     while(month != 0) {
         println("Enter the number of the month (0 to finish)")
@@ -32,9 +33,26 @@ fun exercise1(){
             // https://kotlinlang.org/docs/reference/control-flow.html
             // take into account that some months have 30, others 31 and one 28 or 29.
             // Invoke the function from main()
+            when(month){
+                1,3,5,7,8,10,12->{
+                    println("This month has got 31 days")
+                }
+                4,6,9,11->{
+                    println("This month has got 30 days")
+                }
+                2->{
+                    println("This month has got 28 days")
+                }
+                0->{
+                    println("Finish")
+                }
+                else->{
+                    println("Invalid number")
+                }
+            }
         }
     }
-} */
+}
 
 // TODO: 3 Write a function called exercise3()
 // that accepts a  number and returns a String
